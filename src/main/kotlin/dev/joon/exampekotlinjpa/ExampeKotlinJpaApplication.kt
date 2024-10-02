008d7d615d666dcb00b8702ae4cc3896e8c10e76
sijun-yang <yangsijun@bondit.io>
2024-10-02 14:09:52 +0900

    init

---
+++ src/main/kotlin/dev/joon/exampekotlinjpa/ExampeKotlinJpaApplication.kt
@@ -0,0 +1,11 @@
+package dev.joon.exampekotlinjpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExampeKotlinJpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<ExampeKotlinJpaApplication>(*args)
+}
---
+++ src/test/kotlin/dev/joon/exampekotlinjpa/ExampeKotlinJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.joon.exampekotlinjpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExampeKotlinJpaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
